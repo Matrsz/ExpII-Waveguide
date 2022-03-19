3ea20bb28e13e0d7292bb6d584801327c1347d8b
Matias Roqueta <matias.roqueta.sz@gmail.com>
2022-03-19 13:12:31 -0300

    diagrama de smith, python style

--- Python/analizar.py
+++ Python/analizar.py
@@ -1,3 +1,4 @@
+from operator import mod
 import numpy as np
 import csv
 import matplotlib.pyplot as plt
@@ -47,7 +48,7 @@ def sort_arrays(z, v):
     return [z[args], v[args]]
 
 
-data2 = np.genfromtxt('corneta_relento.csv', delimiter=' ')
+data2 = np.genfromtxt('aislante_relento.csv', delimiter=' ')
 z2, v2 = sort_arrays(data2[:,0], data2[:, 1])
 
 z2 = z2 + 0.5958336695473103
@@ -64,7 +65,7 @@ lg = 2/params["freq"]
 
 delta = (params["phase"]/params["omega"])
 
-print(f"vmax: {vmax*1e3}\n vmin: {vmin*1e3}\n lambda: {lg}\n delta: {delta}\n delta/lg = {delta/lg}")
+print(f"vmax: {vmax*1e3} \nvmin: {vmin*1e3} \nlambda: {lg} \ndelta: {delta} \ndelta/lg = {delta/lg}")
 
 roe = vmax/vmin
 gamma_abs = (roe-1)/(roe+1)
@@ -74,11 +75,44 @@ co = 299792458*100
 f = 10.53e9
 Zo = Zv/co*f*lg
 
-print(f"roe: {roe}\n Zo = {Zo}")
+print(f"roe: {roe} \nZo = {Zo}")
+
+mod_gamma = (roe - 1)/(roe + 1)
+fase_gamma = params["phase"]-np.pi
+gamma = mod_gamma * np.exp(1j*fase_gamma)
+
+zl = (1+gamma)/(1-gamma)
+Zl = zl*Zo
+
+print(f"coef reflexion: {gamma} \nZL = {Zl} = {np.abs(Zl)}*exp({np.angle(Zl)}j)")
+
 
 x = -params["amp"]*np.cos(params["omega"]*z0-params["phase"])+params["offset"]
 plt.plot(z2, v2, z0, x, 'b:', z0, x*0+params["offset"], '--k')
 plt.plot([0, 0], [0, vmax], 'k', [delta, delta], [0, vmax], '--k')
 plt.gca().invert_xaxis()
-plt.show()
+#plt.show()
 
+zg = (Zl+1j*Zo*np.tan(z0))/(Zo+1j*Zl*np.tan(z0))
+yg = 1./zg
+
+
+fig, axs = plt.subplots(2,1)
+
+rg, xg = np.real(zg), np.imag(zg)
+gg, bg = np.real(yg), np.imag(yg)
+
+index = np.nonzero([z if 0.99 < g < 1.01 else 0 for z, g in zip(z0, gg)])[0][0]
+z_1 = z0[index]
+
+print(f"Pos. adaptador: {z_1} \nAdmitancia adaptador: {np.conj(yg[index])}")
+
+axs[0].plot(z0, np.real(zg), z0, np.imag(zg), 'b', z0, z0*0+1, '--k')
+axs[0].plot([0, 0], [min(xg), max(rg)], 'k', [z_1, z_1], [min(xg), max(rg)], '--k')
+axs[0].invert_xaxis()
+
+axs[1].plot(z0, np.real(yg), z0, np.imag(yg), 'b', z0, z0*0+1, '--k')
+axs[1].plot([0, 0], [min(bg), max(gg)], 'k', [z_1, z_1], [min(bg), max(gg)], '--k')
+axs[1].invert_xaxis()
+
+plt.show()
