75d962988f8bbbe1c93cc78102cf9b023a3330be
Matias Roqueta <matias.roqueta.sz@gmail.com>
2022-03-24 21:19:13 -0300

    template informe

--- Arduino/ArduinoOutput/core/WString.cpp.d
+++
@@ -1,3 +0,0 @@
-/home/inox/Documents/IB/4to\ Nivel/Experimental\ II/Guia\ de\ Ondas/Arduino/ArduinoOutput/core/WString.cpp.o: \
- /home/inox/Downloads/arduino-1.8.16/hardware/arduino/avr/cores/arduino/WString.cpp \
- /home/inox/Downloads/arduino-1.8.16/hardware/arduino/avr/cores/arduino/WString.h
